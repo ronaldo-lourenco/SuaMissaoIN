281ece88296b6bc09a6799fe519cc13b83257acb
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-07-04 22:03:29 +0000

    Fiz alterações

---
+++ calcula_media.js
@@ -0,0 +1,6 @@
+const caixaMedia = document.querySelector(".caixa-media") ;
+const nota1= 30 ;
+const nota2= 40 ;
+const nota3= 50 ;
+const media = (nota1+nota2+nota3)/3 ;
+caixaMedia.textContent = "A média de Luiz é "+media ;
