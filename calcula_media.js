c6813fb2f7cc5fad2e652465f6c8d46a92edff2f
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-08-29 22:43:28 +0000

    Atualizei os arquivos Index, Script, Inclui e calcula_media

--- calcula_media.js
+++ calcula_media.js
@@ -1,6 +1,6 @@
 const caixaMedia = document.querySelector(".caixa-media") ;
-const nota1= 30 ;
-const nota2= 40 ;
+const nota1= 80 ;
+const nota2= 80 ;
 const nota3= 50 ;
 const media = (nota1+nota2+nota3)/3 ;
 caixaMedia.textContent = "A média de Luiz é "+media ;
--- script.js
+++ script.js
@@ -8,23 +8,80 @@ const perguntas = [
     {
         enunciado: "A disseminação de fake news pode ter graves consequências para a sociedade,  propagação de boatos, aumento da polarização política e o enfraquecimento da confiança nas instituições e na mídia. Você tem ciência dessas consequências ao compartilhar informações?",
         alternativas: [
-            "Não me preocupo com isso",
-            "Sim, com toda a certeza!"
+            {
+                texto:     "Não me preocupo com isso",
+                afirmacao: "Acredito na liberdade de expressão, todos têm o direito de expor ideias: sejam verdadeiras ou não." 
+            },    
+            {
+                texto:     "Sim, com toda a certeza!" ,
+                afirmacao: "Além de toda as consequências que  a propagação de desinformação podem causar, há também a questão jurídica, pois dependendo da informação disseminada: pode caber processo."
+            }    
         ]
     },
     {
         enunciado: "Como a desigualdade social afeta o acesso  a educação de qualidade?",
         alternativas: [
-            "Faz com que pessoas com maior poder aquisitivo tenha uma educação de maior qualidade.",
-            "Ela não afeta a educação,  pois todos têm acesso aos mesmos recursos."
+            {
+                texto:     "Faz com que pessoas com maior poder aquisitivo tenha uma educação de maior qualidade.",
+                afirmacao: "Pois, é um problema socioeconômico, no qual quem tem mais recursos tem acesso a tempo, cultura, e materiais de qualidade."
+            },
+            {
+                texto:     "Ela não afeta a educação,  pois todos têm acesso aos mesmos recursos.",
+                afirmacao: "Independente da condição social, com esforço (hardwork) todos podem alcançar seus objetivos."
+            }    
         ]
     },
     {
         enunciado: "Quais as causas das desigualdades sociais? A educação pode conscientizar sobre Fake News e Desigualdade Social?",
         alternativas: [
-            "Distribuição de renda e baixa qualificação profissional. A educação pode ser a ferramenta para conscientizar a sociedade.",
-            "Falta de acessibilidade e utilização adequada da tecnologia para a qualificação profissional. A educação não interfere na conscientização da sociedade."
+            {
+                texto:     "Distribuição de renda e baixa qualificação profissional. A educação pode ser a ferramenta para conscientizar a sociedade.",
+                afirmacao: "O problema é  político, social e econômico com foco na  má distribuição de renda, consequentemente inoportunidades na  formação de qualificação, tanto profissional quanto pessoal."
+            }, 
+            {   
+                texto:     "Falta de acessibilidade e utilização adequada da tecnologia para a qualificação profissional. A educação não interfere na conscientização da sociedade.",
+                afirmacao: "Dificuldade em conciliar trabalho com estudo. As barreiras que ainda existem em relação ao acesso a conexão e equipamentos que permitam essa tal formação."
+            }   
         ]
     }
 ] ;
 
+let pergAtual = 0 ;
+let perguntaAtual ;
+let historiaFinal = "" ;
+
+function mostraPergunta(){
+    if (pergAtual >= perguntas.length){
+        mostraResultado() ;
+        return ;
+    } ;
+    perguntaAtual                 = perguntas[pergAtual] ;
+    caixaPerguntas.textContent    = perguntaAtual.enunciado ;
+    caixaAlternativas.textContent = "" ;
+    textoResultado.textContent    = "" ;
+    mostraAlternativas() ;
+} ;
+
+mostraPergunta() ;
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button") ;
+        botaoAlternativas.textContent = alternativa.texto ;
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa)) ;
+        caixaAlternativas.appendChild(botaoAlternativas) ;
+    }
+} ;
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao ;
+    historiaFinal += afirmacao + " " ;
+    pergAtual++ ;
+    mostraPergunta() ;
+} ;
+
+function mostraResultado(){
+    caixaPerguntas.textContent    = "Em 2049..." ;
+    textoResultado.textContent    = historiaFinal ;
+    caixaAlternativas.textContent = "" ;
+}
